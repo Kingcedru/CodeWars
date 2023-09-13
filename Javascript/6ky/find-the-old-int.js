caac3c736042964d8eddd6925a2d6c0bfda83d88
Kingcedru <byishimocedrick@gmail.com>
2023-09-13 11:18:09 +0200

    odd int

---
+++ Javascript/6ky/find-the-old-int.js
@@ -0,0 +1,29 @@
+// Given an array of integers, find the one that appears an odd number of times.
+
+// There will always be only one integer that appears an odd number of times.
+
+// Examples
+// [7] should return 7, because it occurs 1 time (which is odd).
+// [0] should return 0, because it occurs 1 time (which is odd).
+// [1,1,2] should return 2, because it occurs 1 time (which is odd).
+// [0,1,0,1,0] should return 0, because it occurs 3 times (which is odd).
+// [1,2,2,3,3,3,4,3,3,3,2,2,1] should return 4, because it appears 1 time (which is odd).
+
+
+
+function findOdd(A) {
+    let obj = {}
+    for(let x = 0; x < A.length; x++){
+        if(obj.hasOwnProperty(A[x])){
+            obj[A[x]]++
+        }
+        else{
+            obj[A[x]] = 1
+        }
+    }
+    for (const key in obj){
+        if(obj[key] % 2 === 1){
+            return parseInt(key) 
+        }
+    }
+}
